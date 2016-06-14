fd93663693e486a186351d02fabafae650cc3184
PhilBrockman <philbrockman@Phils-MacBook-Pro.local>
2016-06-14 16:51:49 -0400

    .gitignore is a thing
    also, working adding authentication

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+# for authentication
+gem 'devise'
 # Use sqlite3 as the database for Active Record
   gem 'pg', '~> 0.18.4'
   #mailer -- mailgun
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  
+  def authenticate
+      case request.format
+      when Mime::XML, Mime::ATOM
+        if user = authenticate_with_http_basic { |u, p| @account.users.authenticate(u, p) }
+          @current_user = user
+        else
+          request_http_basic_authentication
+        end
+      else
+        if session_authenticated?
+          @current_user = @account.users.find(session[:authenticated][:user_id])
+        else
+          redirect_to(login_url) and return false
+        end
+      end
+    end
 end
