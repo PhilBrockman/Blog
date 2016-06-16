01e409640724808b0312ab1a0f96e254f3c43ebd
PhilBrockman <philbrockman@Phils-MacBook-Pro.local>
2016-06-16 14:46:01 -0400

    bug fix

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -7,8 +7,8 @@ class TeacherMailer < ApplicationMailer
     mail(to: "phil.brockman@gmail.com", subject: 'Welcome to My Awesome Site')
   end
 
-  def send_info(email, credentials)
-  	@credentials = credentials
+  def send_info(email, teacher)
+  	@teacher = teacher
   	mail(to: email, subject: 'KIPP -- Certification Information')
   end
 
